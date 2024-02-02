3d0f136a6c35b2295aad18831447e953271c41ea
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-02-02 14:20:58 +0100

    animation

--- htdocs/JS/script.js
+++ htdocs/JS/script.js
@@ -2,7 +2,18 @@ let fight = document.querySelector('#fight')
 
 fight.addEventListener('click', function(){
 
-    alert('attack')
+    document.querySelector('#coupDePoing').play()
     
+    let fireBall = document.querySelector('#fireBall').classList
+
+    fireBall.toggle("fireBallHidden")
+    fireBall.toggle("fireBall")
+
+    let shuriken = document.querySelector('#shuriken').classList
+   
+    shuriken.toggle("shurikenHidden")
+    shuriken.toggle("shuriken")
+
+
 
 })
